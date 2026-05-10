79867d389ea10d17ede506f24372f675f24f5a83
Vasiliy Markitan <mkt.dev@yandex.ru>
2026-05-10 18:54:24 +0300

    update business logic

--- src/test/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
-import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.ArrayList;
@@ -9,7 +8,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-@Component
 public class InMemoryFilmStorage implements FilmStorage {
     private final Map<Long, Film> films = new HashMap<>();
 
--- src/test/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
-import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.ArrayList;
@@ -9,7 +8,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-@Component
 public class InMemoryUserStorage implements UserStorage {
     private final Map<Long, User> users = new HashMap<>();
 
